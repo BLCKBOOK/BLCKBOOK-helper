0eefcd78f3cd87a53161fbab6e08a00b1b2db9cc
NielsCo <89005445+NielsCo@users.noreply.github.com>
2021-12-09 14:11:24 +0100

    Added voter-money-pool. Tzip16 views and tests for them

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import {MichelsonMap, TezosToolkit} from '@taquito/taquito';
 import {faucet} from './faucet';
 import {importKey} from '@taquito/signer';
+import {tzip16, Tzip16Module} from '@taquito/tzip16';
 
 const Tezos = new TezosToolkit('https://hangzhounet.api.tez.ie');
-
+Tezos.addExtension(new Tzip16Module());
 
 importKey(
     Tezos,
@@ -15,9 +16,15 @@ importKey(
 
 const tokenContractAddress = 'KT1QALmg4PfA1ZDNYWhmWXMNpZhdwLdNbfMz';
 
-const genericMultisigJSONfile = require('../dist/contract.json');
+const fa2ContractMichelsonCode = require('../dist/contract.json');
+const voterMoneyPoolMichelsonCode = require('../dist/voter_money_pool_contract.json');
 const adminPublicKey = faucet.pkh;
 const initialFA2Storage = `(Pair "tz1PEbaFp9jE6syH5xg29YRegbwLLehzK3w2" (Pair 0 (Pair {} (Pair {} (Pair {} (Pair False {}))))))`;
+const initialVoterMoneyPoolStorage = '(Pair "tz1PEbaFp9jE6syH5xg29YRegbwLLehzK3w2" (Pair {} (Pair {} {})))'
+const contractMetadataIpfsKey = 'QmaXB89rnWPU9x2cDzHEy5YPdoK9epzRqgc7Lv8bvUv6ck'
+const ipfsPrefix = 'ipfs://'
+const voterMoneyPoolMetadataIpfsKey = 'QmVPo1mxMTFSWcmFARMzdt6ieaYuvZj73nQKHNUPaBKsKY'
+const voterMoneyPoolContractAddress = 'KT1WdC4Qy9p2Wixfs2M16iwnauUXGAaAZJAV'
 
 function originate(code: any, initialStorage: string) {
     Tezos.contract
@@ -103,8 +110,50 @@ async function getCurrentTokenIndex(): Promise<number> {
     return (storage as any).all_tokens.toNumber() as number;
 }
 
-getCurrentTokenIndex().then(tokenIndex => {
+async function setContractMetaData(contractAddress: string, contractIpfsMetadata: string) {
+    const contract = await Tezos.contract.at(contractAddress);
+    contract.methodsObject.set_metadata({
+        k: '',
+        v: char2Bytes(ipfsPrefix + contractIpfsMetadata)
+    }).send().then((op) => {
+        console.log(`Waiting for ${op.hash} to be confirmed...`);
+        return op.confirmation(3).then(() => op.hash);
+    })
+        .then((hash) => console.log(`Operation injected: https://hangzhou.tzstats.com/${hash}`))
+        .catch((error) => console.log(`Error: ${JSON.stringify(error, null, 2)}`));
+}
+
+/*getCurrentTokenIndex().then(tokenIndex => {
     for (let i = 0; i < 2; i++) {
         mint('no real link... sorry', i + tokenIndex, adminPublicKey);
     } // this does not work to mint multiple tokens as the number isn't updated in the storage, when the contract-call is made
-});
+});*/
+
+async function testTzip16Metadata(contractAddress: string) {
+    const contract = await Tezos.contract.at(contractAddress, tzip16)
+    const metadata = await contract.tzip16().getMetadata();
+    const views = await contract.tzip16().metadataViews();
+    console.log(metadata);
+    console.log(views);
+}
+
+/**
+ * Get's the contract, it's views and then returns the count_tokens view
+ * @ret the count_tokens view which is the current token_index
+ */
+async function getCountTokensFromView(): Promise<number> {
+    const contract = await Tezos.contract.at(tokenContractAddress, tzip16)
+    const views = await contract.tzip16().metadataViews();
+    const ret = (await views.count_tokens().executeView()).toNumber();
+    return ret;
+}
+
+async function getAmountInMoneyPool(): Promise<number> {
+    const contract = await Tezos.contract.at(voterMoneyPoolContractAddress, tzip16)
+    const views = await contract.tzip16().metadataViews();
+    const ret = (await views.get_balance().executeView(adminPublicKey)).toNumber();
+    console.log(ret);
+    return ret;
+}
+
+getAmountInMoneyPool();
